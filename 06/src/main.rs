3376c5f3c167ceb800ff95cfec5dc28bc2de1761
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-12-06 06:55:28 +0000

    Day 6 - part 2

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(iterator_fold_self)]
 use std::collections::HashSet;
 
 fn parse_input() -> Vec<Vec<&'static str>> {
@@ -20,8 +21,23 @@ fn part1() -> usize {
         .sum()
 }
 
+fn part2() -> usize {
+    parse_input()
+        .iter()
+        .map(|group| {
+            group
+                .iter()
+                .map(|person| person.chars().collect::<HashSet<_>>())
+                .fold_first(|a, b| a.intersection(&b).cloned().collect())
+                .unwrap()
+                .len()
+        })
+        .sum()
+}
+
 fn main() {
     println!("part 1: {}", part1());
+    println!("part 2: {}", part2());
 }
 
 #[cfg(test)]
@@ -30,5 +46,9 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(), 6686)
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 3476)
     }
 }
