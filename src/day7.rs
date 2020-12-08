b5483aa9b98343405fff175d55f31482669df086
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-12-08 17:40:09 +0000

    Perf: Use enums to save a ms in generating data for Day 7 - part 1
    Using strings as keys in the graph meant you had to copy more data than
    necessary. Enums are a more compact and expressive representation of the same data. It also allows for stricter input validation.
    With this, all generations go sub-millisecond.
    
    ```
    cargo aoc bench -g
    ```
    
    ```
    Day7 - Part1/(default)  time:   [525.39 us 527.35 us 529.70 us]
                            change: [-3.2365% -2.2645% -1.3345%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 14 outliers among 100 measurements (14.00%)
      3 (3.00%) high mild
      11 (11.00%) high severe
    
    Day7 - Part2/(default)  time:   [588.18 ns 591.01 ns 595.06 ns]
                            change: [-13.954% -13.394% -12.811%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 9 outliers among 100 measurements (9.00%)
      1 (1.00%) high mild
      8 (8.00%) high severe
    
    Generator Day7/(default)
                            time:   [804.65 us 806.05 us 807.60 us]
                            change: [-61.168% -60.462% -59.817%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 8 outliers among 100 measurements (8.00%)
      1 (1.00%) high mild
      7 (7.00%) high severe
    
    Generator Day7/(default)
                            time:   [807.57 us 810.01 us 812.72 us]
                            change: [+0.2538% +1.3125% +2.6886%] (p = 0.02 < 0.05)
                            Change within noise threshold.
    Found 13 outliers among 100 measurements (13.00%)
      8 (8.00%) high mild
      5 (5.00%) high severe
    ```

--- src/day7.rs
+++ src/day7.rs
@@ -3,20 +3,108 @@ use itertools::Itertools;
 use petgraph::{algo, prelude::*};
 use std::collections::HashMap;
 use std::str::FromStr;
+use strum_macros::EnumString;
 
 #[aoc_generator(day7)]
 fn parse_input(data: &str) -> BagRules {
     BagRules::from_str(data).unwrap()
 }
 
+#[derive(Eq, PartialEq, Clone, Copy, Hash, EnumString)]
+#[strum(serialize_all = "snake_case")]
+enum Adjective {
+    Bright,
+    Clear,
+    Dark,
+    Dim,
+    Dotted,
+    Drab,
+    Dull,
+    Faded,
+    Mirrored,
+    Muted,
+    Light,
+    Pale,
+    Plaid,
+    Posh,
+    Shiny,
+    Striped,
+    Vibrant,
+    Wavy,
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Hash, EnumString)]
+#[strum(serialize_all = "snake_case")]
+enum Color {
+    Aqua,
+    Beige,
+    Black,
+    Blue,
+    Bronze,
+    Brown,
+    Chartreuse,
+    Coral,
+    Crimson,
+    Cyan,
+    Gold,
+    Gray,
+    Green,
+    Fuchsia,
+    Indigo,
+    Lavender,
+    Lime,
+    Magenta,
+    Maroon,
+    Olive,
+    Orange,
+    Plum,
+    Purple,
+    Red,
+    Salmon,
+    Silver,
+    Tan,
+    Teal,
+    Tomato,
+    Turquoise,
+    Violet,
+    White,
+    Yellow,
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Hash)]
+struct Bag {
+    adjective: Adjective,
+    color: Color,
+}
+
+impl FromStr for Bag {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (adjective, color) = s
+            .strip_suffix(" bags.")
+            .or_else(|| s.strip_suffix(" bag."))
+            .or_else(|| s.strip_suffix(" bags"))
+            .or_else(|| s.strip_suffix(" bag"))
+            .unwrap()
+            .splitn(2, ' ')
+            .collect_tuple()
+            .unwrap();
+        Ok(Bag {
+            adjective: Adjective::from_str(adjective)
+                .map_err(|e| format!("{} for {}", e, adjective))?,
+            color: Color::from_str(color).map_err(|e| format!("{} for {}", e, color))?,
+        })
+    }
+}
+
 struct BagRules {
-    nodes: HashMap<String, NodeIndex>,
-    graph: DiGraph<String, u32>,
+    nodes: HashMap<Bag, NodeIndex>,
+    graph: DiGraph<Bag, u32>,
 }
 
 fn digraph_from_weighted_edges(
-    weighted_edges: &[(String, String, u32)],
-) -> (HashMap<String, NodeIndex>, DiGraph<String, u32>) {
+    weighted_edges: &[(Bag, Bag, u32)],
+) -> (HashMap<Bag, NodeIndex>, DiGraph<Bag, u32>) {
     let mut graph = DiGraph::new();
     let mut nodes = HashMap::new();
     for (input, output, weight) in weighted_edges {
@@ -40,22 +128,12 @@ impl FromStr for BagRules {
             .filter(|line| !line.contains("no other bags."))
             .map(|line| line.split(" contain ").collect_tuple().unwrap())
             .flat_map(|(container, all_content)| {
-                let container = container.strip_suffix(" bags").unwrap();
                 all_content.split(", ").map(move |content| {
-                    let content = content
-                        .strip_suffix(" bags.")
-                        .or_else(|| content.strip_suffix(" bag."))
-                        .or_else(|| content.strip_suffix(" bags"))
-                        .or_else(|| content.strip_suffix(" bag"))
-                        .unwrap();
-                    let (quantity, bag) = content
-                        .splitn(2, ' ')
-                        .map(|s| s.to_string())
-                        .collect_tuple()
-                        .unwrap();
+                    let (quantity, content) = content.splitn(2, ' ').collect_tuple().unwrap();
+
                     Ok((
-                        container.to_string(),
-                        bag,
+                        Bag::from_str(container)?,
+                        Bag::from_str(content)?,
                         quantity.parse::<u32>().map_err(|_| {
                             format!("Couldn't parse \"{}\" as a bag quantity", quantity)
                         })?,
@@ -69,8 +147,8 @@ impl FromStr for BagRules {
 }
 
 impl BagRules {
-    fn count_types_of_bags_which_can_contain(&self, target: &str) -> usize {
-        let target = self.nodes[&target.to_string()];
+    fn count_types_of_bags_which_can_contain(&self, target: &Bag) -> usize {
+        let target = self.nodes[target];
         self.graph
             .node_indices()
             .filter(|node| *node != target)
@@ -83,20 +161,25 @@ impl BagRules {
             .map(|edge| *edge.weight() as usize * (1 + self.accumulate_edge_weights(edge.target())))
             .sum()
     }
-    fn count_bags_which_must_be_contained(&self, target: &str) -> usize {
-        let target = self.nodes[&target.to_string()];
+    fn count_bags_which_must_be_contained(&self, target: &Bag) -> usize {
+        let target = self.nodes[target];
         self.accumulate_edge_weights(target)
     }
 }
 
+const SHINY_GOLD: Bag = Bag {
+    adjective: Adjective::Shiny,
+    color: Color::Gold,
+};
+
 #[aoc(day7, part1)]
 fn part1(bag_rules: &BagRules) -> usize {
-    bag_rules.count_types_of_bags_which_can_contain("shiny gold")
+    bag_rules.count_types_of_bags_which_can_contain(&SHINY_GOLD)
 }
 
 #[aoc(day7, part2)]
 fn part2(bag_rules: &BagRules) -> usize {
-    bag_rules.count_bags_which_must_be_contained("shiny gold")
+    bag_rules.count_bags_which_must_be_contained(&SHINY_GOLD)
 }
 
 #[cfg(test)]
@@ -115,10 +198,8 @@ dotted black bags contain no other bags.";
     #[test]
     fn example() {
         let rules = BagRules::from_str(EXAMPLE_INPUT).unwrap();
-        assert_eq!(4, rules.count_types_of_bags_which_can_contain("shiny gold"));
-        assert_eq!(7, rules.count_types_of_bags_which_can_contain("faded blue"));
-        assert_eq!(0, rules.count_types_of_bags_which_can_contain("light red"));
-        assert_eq!(32, rules.count_bags_which_must_be_contained("shiny gold"));
+        assert_eq!(4, rules.count_types_of_bags_which_can_contain(&SHINY_GOLD));
+        assert_eq!(32, rules.count_bags_which_must_be_contained(&SHINY_GOLD));
     }
     fn input() -> BagRules {
         parse_input(include_str!("../input/2020/day7.txt"))
