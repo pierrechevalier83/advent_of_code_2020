6ac97f1e91adda8479f0f730ce304d7d8f5aa7ce
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-12-08 20:46:42 +0000

    Perf: Optimize away HashSets from Day 6 - part 2
    For part 1: replace the HashSet by a BTreeSet as it was annedoctaly
    faster.
    
    For part 2: replace the HashSet intersections with a Vec<bool> relying on fact that
    the number of letters is finite.
    
    ```
    cargo aoc bench -g -d6
    ```
    
    ```
    Day6 - Part1/(default)  time:   [556.00 us 557.21 us 558.63 us]
                            change: [-1.0364% -0.6995% -0.3406%] (p = 0.00 < 0.05)
                            Change within noise threshold.
    Found 9 outliers among 100 measurements (9.00%)
      4 (4.00%) high mild
      5 (5.00%) high severe
    
    Day6 - Part2/(default)  time:   [643.31 us 644.55 us 645.93 us]
                            change: [-58.508% -58.289% -58.089%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 3 outliers among 100 measurements (3.00%)
      3 (3.00%) high mild
    
    Generator Day6/(default)
                            time:   [292.93 us 293.71 us 294.60 us]
                            change: [-4.9273% -4.4139% -3.8875%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 10 outliers among 100 measurements (10.00%)
      6 (6.00%) high mild
      4 (4.00%) high severe
    
    Generator Day6/(default)
                            time:   [292.16 us 292.75 us 293.40 us]
                            change: [-0.8966% -0.3374% +0.2540%] (p = 0.25 > 0.05)
                            No change in performance detected.
    Found 10 outliers among 100 measurements (10.00%)
      2 (2.00%) low mild
      3 (3.00%) high mild
      5 (5.00%) high severe
    ```

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 
 #[aoc_generator(day6)]
 fn parse_input(data: &str) -> Vec<Vec<String>> {
@@ -8,6 +8,8 @@ fn parse_input(data: &str) -> Vec<Vec<String>> {
         .collect()
 }
 
+const ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+
 #[aoc(day6, part1)]
 fn part1(data: &[Vec<String>]) -> usize {
     data.iter()
@@ -15,7 +17,9 @@ fn part1(data: &[Vec<String>]) -> usize {
             group
                 .iter()
                 .flat_map(|person| person.chars())
-                .collect::<HashSet<char>>()
+                // Benchmarked to be better than a HashSet here, and equivalent to a sorted and
+                // deduplicated Vec
+                .collect::<BTreeSet<_>>()
                 .len()
         })
         .sum()
@@ -23,14 +27,28 @@ fn part1(data: &[Vec<String>]) -> usize {
 
 #[aoc(day6, part2)]
 fn part2(data: &[Vec<String>]) -> usize {
+    let mut chars_present: Vec<bool> = ALPHABET.chars().map(|_| true).collect();
     data.iter()
         .map(|group| {
-            group
-                .iter()
-                .map(|person| person.chars().collect::<HashSet<_>>())
-                .fold_first(|a, b| a.intersection(&b).cloned().collect())
-                .unwrap()
-                .len()
+            chars_present.iter_mut().for_each(|c| *c = true);
+            for person in group.iter() {
+                let mut chars = person.chars().collect::<Vec<_>>();
+                chars.sort();
+                chars.dedup();
+                let mut chars_idx = 0;
+                for (letter, char_present) in ALPHABET.chars().zip(chars_present.iter_mut()) {
+                    if let Some(c) = chars.get(chars_idx) {
+                        if letter != *c {
+                            *char_present = false;
+                        } else {
+                            chars_idx += 1;
+                        }
+                    } else {
+                        *char_present = false;
+                    }
+                }
+            }
+            chars_present.iter().filter(|b| **b).count()
         })
         .sum()
 }
