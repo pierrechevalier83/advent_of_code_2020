033217ed6c05870ed8e32f45f406add4e6290c4f
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-12-15 00:52:43 +0000

    Day 14 - part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,26 +1,79 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use radix_fmt;
+use std::collections::HashMap;
 use std::iter::repeat;
+
+enum Version {
+    V1,
+    V2,
+}
 
 #[derive(Debug, Clone, Copy)]
 struct Mask {
-    and_mask: u64,
-    or_mask: u64,
+    zeroes: u64,
+    ones: u64,
 }
 
 impl From<&str> for Mask {
     fn from(s: &str) -> Self {
         // They describe it as a "mask", but it's really two masks:
-        // Apply bitwise and with all the ones (where X means 1)
-        // Apply bitwise or with all the zeroes (where X means 0)
-        let and_mask = u64::from_str_radix(&format!("{:1>64}", s).replace('X', "1"), 2).unwrap();
-        let or_mask = u64::from_str_radix(&format!("{:0>64}", s).replace('X', "0"), 2).unwrap();
-        Self { and_mask, or_mask }
+        let zeroes = !u64::from_str_radix(&format!("{:1>64}", s).replace('X', "1"), 2).unwrap();
+        let ones = u64::from_str_radix(&format!("{:0>64}", s).replace('X', "0"), 2).unwrap();
+        Self { zeroes, ones }
     }
 }
 
 impl Mask {
-    fn apply_to(&self, x: u64) -> u64 {
-        x & self.and_mask | self.or_mask
+    fn apply_v1(&self, x: u64) -> u64 {
+        x & !self.zeroes | self.ones
+    }
+    fn floating_mask(&self) -> u64 {
+        // ones for all unset bits
+        // Example:
+        // original: xxx1x0xx00011;
+        // ones:     0001000000011;
+        // !ones:    1110111111100;
+        // zeroes:   1111101100011;
+        // output    1110101100000
+        let floating = (!self.zeroes & !self.ones) & (!0 >> (64 - 36));
+        floating
+    }
+    fn bits(x: usize) -> String {
+        format!("{}", radix_fmt::radix(x, 2))
+    }
+    fn make_nth_mask(n: usize, floating_bits_indices: &[usize]) -> usize {
+        let mask = floating_bits_indices
+            .iter()
+            .enumerate()
+            .map(|(index, floating_bits_index)| {
+                let bit_from_n = n >> index & 1;
+                bit_from_n << floating_bits_index
+            })
+            .fold(0, |acc, x| acc | x);
+        mask
+    }
+    fn make_all_masks(&self) -> impl Iterator<Item = usize> {
+        let floating_bits = Self::bits(self.floating_mask() as usize);
+        let floating_bits_indices = floating_bits
+            .chars()
+            .rev()
+            .enumerate()
+            .filter_map(|(index, bit)| if bit == '1' { Some(index) } else { None })
+            .collect::<Vec<_>>();
+        (0..2_usize.pow(floating_bits_indices.len() as u32))
+            .map(move |n| Self::make_nth_mask(n, &floating_bits_indices))
+    }
+    fn apply_v2(&self, address: usize) -> Vec<usize> {
+        let floating_mask = self.floating_mask() as usize;
+        let ones = self.ones as usize;
+        self.make_all_masks()
+            .map(|mask| {
+                let with_ones = address | ones;
+                let with_cleared_floating_mask = with_ones & !floating_mask;
+                let with_this_mask = with_cleared_floating_mask | mask;
+                with_this_mask
+            })
+            .collect()
     }
 }
 
@@ -49,7 +102,7 @@ impl From<&str> for Op {
 struct Program {
     ops: Vec<Op>,
     mask: Mask,
-    mem: Vec<u64>,
+    mem: HashMap<usize, u64>,
 }
 
 impl From<&str> for Program {
@@ -59,36 +112,37 @@ impl From<&str> for Program {
             .split_terminator('\n')
             .map(|line| line.into())
             .collect::<Vec<_>>();
-        let max_address = ops
-            .iter()
-            .filter_map(|op| {
-                if let Op::Write(address, _) = op {
-                    Some(*address)
-                } else {
-                    None
-                }
-            })
-            .max()
-            .unwrap_or(0);
         Self {
             mask,
             ops,
-            mem: repeat(0).take(max_address + 1).collect(),
+            mem: HashMap::new(),
         }
     }
 }
 
 impl Program {
-    fn run(&mut self) {
-        for op in self.ops.iter() {
-            match op {
-                Op::UpdateMask(m) => self.mask = *m,
-                Op::Write(address, value) => self.mem[*address] = self.mask.apply_to(*value),
+    fn run(&mut self, version: Version) {
+        for op in self.ops.clone().iter() {
+            match version {
+                Version::V1 => match op {
+                    Op::UpdateMask(m) => self.mask = *m,
+                    Op::Write(address, value) => {
+                        *self.mem.entry(*address).or_insert(0) = self.mask.apply_v1(*value);
+                    }
+                },
+                Version::V2 => match op {
+                    Op::UpdateMask(m) => self.mask = *m,
+                    Op::Write(address, value) => {
+                        self.mask.apply_v2(*address).iter().for_each(|address| {
+                            *self.mem.entry(*address).or_insert(0) = *value;
+                        })
+                    }
+                },
             }
         }
     }
     fn mem_sum(&self) -> u64 {
-        self.mem.iter().sum()
+        self.mem.values().sum()
     }
 }
 
@@ -100,7 +154,14 @@ fn parse_input(s: &str) -> Program {
 #[aoc(day14, part1)]
 fn part1(prog: &Program) -> u64 {
     let mut prog = prog.clone();
-    prog.run();
+    prog.run(Version::V1);
+    prog.mem_sum()
+}
+
+#[aoc(day14, part2)]
+fn part2(prog: &Program) -> u64 {
+    let mut prog = prog.clone();
+    prog.run(Version::V2);
     prog.mem_sum()
 }
 
@@ -113,12 +174,25 @@ mod tests {
     #[test]
     fn test_mask() {
         let mask: Mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X".into();
-        assert_eq!(73, mask.apply_to(11));
-        assert_eq!(101, mask.apply_to(101));
-        assert_eq!(64, mask.apply_to(0));
+        assert_eq!(73, mask.apply_v1(11));
+        assert_eq!(101, mask.apply_v1(101));
+        assert_eq!(64, mask.apply_v1(0));
     }
     #[test]
     fn test_part1() {
         assert_eq!(part1(&input()), 7440382076205)
+    }
+    #[test]
+    fn test_example_part2() {
+        assert_eq!(
+            part2(&parse_input(
+                "mask = 000000000000000000000000000000X1001X\nmem[42] = 100\nmask = 00000000000000000000000000000000X0XX\nmem[26] = 1"
+            )),
+            208
+        )
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&input()), 4200656704538)
     }
 }
