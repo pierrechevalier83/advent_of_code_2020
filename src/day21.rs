6323e4214f01ba4d57baee5ad46f17efe0cf0766
Pierre Chevalier <pierrechevalier83@gmail.com>
2020-12-21 08:55:30 +0000

    Day 21 - part 2

--- src/day21.rs
+++ src/day21.rs
@@ -28,16 +28,16 @@ use std::collections::{BTreeMap, HashSet};
 // Find one that has size one when filtered with mapped
 // Add mapping
 
+type Alergen = String;
 type Food = String;
-type Ingredient = String;
 
 // Note: if performance isn't satisfying, we could keep one mapping of ingredients and foods to
 // indices and operate on ints instead of string everywhere, and replace some BTreeMaps with
 // Vecs where the keys are the indices at which the elements are found.
 #[derive(Debug)]
 struct FoodList {
-    // mapping from ingredients to foods that may contain it
-    may_contain: BTreeMap<Ingredient, HashSet<Food>>,
+    // mapping from alergens to foods that may contain it
+    may_contain: BTreeMap<Alergen, HashSet<Food>>,
     // How many times did we see each ingredient
     counts: BTreeMap<Food, usize>,
 }
@@ -75,23 +75,28 @@ impl From<&str> for FoodList {
 }
 
 impl FoodList {
-    fn num_unmapped_ingredients(&self) -> usize {
+    fn num_unmapped_ingredients_and_mappings(&self) -> (usize, Vec<(Alergen, Food)>) {
         let mut mapped_foods = HashSet::new();
-        while let Some(food) = self.may_contain.iter().find_map(|(_ingredient, foods)| {
+        let mut alergen_mappings = Vec::<(Alergen, Food)>::new();
+        while let Some(food) = self.may_contain.iter().find_map(|(alergen, foods)| {
             let only_possible_foods = foods.difference(&mapped_foods).collect::<Vec<_>>();
             if only_possible_foods.len() == 1 {
-                Some(only_possible_foods[0].clone())
+                let food = only_possible_foods[0];
+                alergen_mappings.push((alergen.clone(), food.clone()));
+                Some(food.clone())
             } else {
                 None
             }
         }) {
             mapped_foods.insert(food);
         }
-        self.counts
+        let num_unmapped_ingredients = self
+            .counts
             .iter()
             .filter(|(food, _)| mapped_foods.get(food.clone()).is_none())
             .map(|(_, count)| count)
-            .sum()
+            .sum();
+        (num_unmapped_ingredients, alergen_mappings)
     }
 }
 
@@ -102,7 +107,18 @@ fn parse_input(s: &str) -> FoodList {
 
 #[aoc(day21, part1)]
 fn part1(foods: &FoodList) -> usize {
-    foods.num_unmapped_ingredients()
+    foods.num_unmapped_ingredients_and_mappings().0
+}
+
+#[aoc(day21, part2)]
+fn part2(foods: &FoodList) -> String {
+    let mut mappings = foods.num_unmapped_ingredients_and_mappings().1;
+    mappings.sort_by(|(left_alergen, _), (right_alergen, _)| left_alergen.cmp(right_alergen));
+    mappings
+        .iter()
+        .map(|(_, food)| food.to_owned())
+        .intersperse(",".to_string())
+        .collect::<String>()
 }
 
 #[cfg(test)]
@@ -122,5 +138,12 @@ sqjhc mxmxvkd sbzzf (contains fish)";
     #[test]
     fn test_part1() {
         assert_eq!(2150, part1(&input()))
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            "vpzxk,bkgmcsx,qfzv,tjtgbf,rjdqt,hbnf,jspkl,hdcj",
+            part2(&input())
+        )
     }
 }
