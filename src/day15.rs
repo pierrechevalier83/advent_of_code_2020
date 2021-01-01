f67c08b2db60f87688c1fd0e097bbe7a2c96df1f
Pierre Chevalier <pierrechevalier83@gmail.com>
2021-01-01 13:56:08 +0000

    Perf: Optimize Day 15 (twice as fast with Vec<Option>)

--- src/day15.rs
+++ src/day15.rs
@@ -1,8 +1,8 @@
 use aoc_runner_derive::aoc;
-use rustc_hash::FxHashMap;
+use std::iter::repeat;
 
-fn puzzle_input() -> Game {
-    Game::from(vec![9, 6, 0, 10, 18, 2, 1])
+fn puzzle_input(max_size: usize) -> Game {
+    Game::from(vec![9, 6, 0, 10, 18, 2, 1], max_size)
 }
 
 #[derive(Debug)]
@@ -10,22 +10,23 @@ struct Game {
     len: usize,
     last: usize,
     turn: usize,
-    previously_seen: FxHashMap<usize, usize>,
+    previously_seen: Vec<Option<usize>>,
 }
 
-impl From<Vec<usize>> for Game {
-    fn from(seq: Vec<usize>) -> Self {
+impl Game {
+    fn from(seq: Vec<usize>, max_size: usize) -> Self {
+        let mut previously_seen = repeat(None).take(max_size).collect::<Vec<_>>();
+        seq.iter()
+            .take(seq.len() - 1)
+            .enumerate()
+            .for_each(|(i, n)| {
+                previously_seen[*n] = Some(i);
+            });
         Self {
             len: seq.len(),
             last: *seq.last().unwrap(),
             turn: seq.len(),
-            previously_seen: seq
-                .iter()
-                .take(seq.len() - 1)
-                .copied()
-                .enumerate()
-                .map(|(i, n)| (n, i))
-                .collect(),
+            previously_seen,
         }
     }
 }
@@ -33,12 +34,10 @@ impl From<Vec<usize>> for Game {
 impl Iterator for Game {
     type Item = usize;
     fn next(&mut self) -> Option<usize> {
-        let next = self
-            .previously_seen
-            .get(&self.last)
+        let next = self.previously_seen[self.last]
             .map(|previously_seen_at| self.turn - 1 - previously_seen_at)
             .unwrap_or(0);
-        self.previously_seen.insert(self.last, self.turn - 1);
+        self.previously_seen[self.last] = Some(self.turn - 1);
         self.turn += 1;
         self.last = next;
         Some(next)
@@ -47,13 +46,13 @@ impl Iterator for Game {
 
 #[aoc(day15, part1)]
 fn part1(_: &str) -> usize {
-    let mut game = puzzle_input();
+    let mut game = puzzle_input(2020);
     game.nth(2020 - game.len - 1).unwrap()
 }
 #[aoc(day15, part2)]
 fn part2(_: &str) -> usize {
-    let mut game = puzzle_input();
-    game.nth(30000000 - game.len - 1).unwrap()
+    let mut game = puzzle_input(30_000_000);
+    game.nth(30_000_000 - game.len - 1).unwrap()
 }
 
 #[cfg(test)]
@@ -61,12 +60,15 @@ mod tests {
     use super::*;
     #[test]
     fn test_examples() {
-        assert_eq!(Some(1), Game::from(vec![1, 3, 2]).nth(2020 - 3 - 1));
-        assert_eq!(Some(10), Game::from(vec![2, 1, 3]).nth(2020 - 3 - 1));
-        assert_eq!(Some(27), Game::from(vec![1, 2, 3]).nth(2020 - 3 - 1));
-        assert_eq!(Some(78), Game::from(vec![2, 3, 1]).nth(2020 - 3 - 1));
-        assert_eq!(Some(438), Game::from(vec![3, 2, 1]).nth(2020 - 3 - 1));
-        assert_eq!(Some(1836), Game::from(vec![3, 1, 2]).nth(2020 - 3 - 1));
+        assert_eq!(Some(1), Game::from(vec![1, 3, 2], 2020).nth(2020 - 3 - 1));
+        assert_eq!(Some(10), Game::from(vec![2, 1, 3], 2020).nth(2020 - 3 - 1));
+        assert_eq!(Some(27), Game::from(vec![1, 2, 3], 2020).nth(2020 - 3 - 1));
+        assert_eq!(Some(78), Game::from(vec![2, 3, 1], 2020).nth(2020 - 3 - 1));
+        assert_eq!(Some(438), Game::from(vec![3, 2, 1], 2020).nth(2020 - 3 - 1));
+        assert_eq!(
+            Some(1836),
+            Game::from(vec![3, 1, 2], 2020).nth(2020 - 3 - 1)
+        );
     }
     #[test]
     fn test_part1() {
